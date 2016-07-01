c32bdf683bc23b78b6c146250d54c5712cd1a712
Casey Watson <casey.j.watson@gmail.com>
2016-07-01 12:24:27 -0500

    Getting my test on.

---
+++ Calculator.Web.Tests/CalculatorTests.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Web.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Should_add_two_numbers()
+        {
+            // 1. Arrange
+
+            double x = 10;
+            double y = 5;
+
+            var expectedResult = (x + y);
+            var calculator = new Calculator();
+
+            // 2. Act
+
+            var actualResult = calculator.Add(x, y);
+
+            // 3. Assert
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Should_subtract_two_numbers()
+        {
+            // 1. Arrange
+
+            double x = 10;
+            double y = 5;
+
+            var expectedResult = (x - y);
+            var calculator = new Calculator();
+
+            // 2. Act
+
+            var actualResult = calculator.Subtract(x, y);
+
+            // 3. Assert
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Should_multiply_two_numbers()
+        {
+            // 1. Arrange
+
+            double x = 10;
+            double y = 5;
+
+            var expectedResult = (x * y);
+            var calculator = new Calculator();
+
+            // 2. Act
+
+            var actualResult = calculator.Multiply(x, y);
+
+            // 3. Assert
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Should_divide_two_numbers()
+        {
+            // 1. Arrange
+
+            double x = 10;
+            double y = 5;
+
+            var expectedResult = (x / y);
+            var calculator = new Calculator();
+
+            // 2. Act
+
+            var actualResult = calculator.Divide(x, y);
+
+            // 3. Assert
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ Calculator.Web.Tests/HomeControllerTests.cs
@@ -0,0 +1,345 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Calculator.Web.Controllers;
+using Calculator.Web.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace Calculator.Web.Tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Should_add_two_numbers()
+        {
+            // 1. Arrange
+
+            double x = 2;
+            double y = 3;
+
+            var result = (x + y);
+            var mockCalculator = new Mock<ICalculator>();
+            var homeController = new HomeController(mockCalculator.Object);
+            var model = new CalculatorModel {X = x.ToString(), Y = y.ToString()};
+
+            mockCalculator.Setup(c => c.Add(x, y)).Returns(result);
+
+            // 2. Act
+
+            var viewResult = (homeController.Add(model) as ViewResult);
+
+            // 3. Assert
+
+            Assert.IsNotNull(viewResult);
+
+            model = (viewResult.Model as CalculatorModel);
+
+            Assert.IsNotNull(model);
+            Assert.AreEqual(model.Result, result.ToString());
+        }
+
+        [Test]
+        public void Should_raise_vaildation_error_when_adding_where_X_is_not_a_number()
+        {
+            // 1. Arrange
+
+            double y = 3;
+
+            var mockCalculator = new Mock<ICalculator>();
+            var homeController = new HomeController(mockCalculator.Object);
+            var calculatorModel = new CalculatorModel { X = "foo", Y = y.ToString() };
+
+            // 2. Act
+
+            var viewResult = (homeController.Add(calculatorModel) as ViewResult);
+
+            // 3. Assert
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(viewResult.ViewData.ModelState.Count, 1);
+
+            var modelState = viewResult.ViewData.ModelState.Values.First();
+
+            Assert.AreEqual(modelState.Errors.Count, 1);
+            Assert.AreEqual(modelState.Errors.First().ErrorMessage, "X must be a number.");
+        }
+
+        [Test]
+        public void Should_raise_vaildation_error_when_adding_where_Y_is_not_a_number()
+        {
+            // 1. Arrange
+
+            double x = 3;
+
+            var mockCalculator = new Mock<ICalculator>();
+            var homeController = new HomeController(mockCalculator.Object);
+            var calculatorModel = new CalculatorModel { X = x.ToString(), Y = "bar" };
+
+            // 2. Act
+
+            var viewResult = (homeController.Add(calculatorModel) as ViewResult);
+
+            // 3. Assert
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(viewResult.ViewData.ModelState.Count, 1);
+
+            var modelState = viewResult.ViewData.ModelState.Values.First();
+
+            Assert.AreEqual(modelState.Errors.Count, 1);
+            Assert.AreEqual(modelState.Errors.First().ErrorMessage, "Y must be a number.");
+        }
+
+        [Test]
+        public void Should_subtract_two_numbers()
+        {
+            // 1. Arrange
+
+            double x = 3;
+            double y = 2;
+            var result = (x - y);
+            var mockCalculator = new Mock<ICalculator>();
+            var homeController = new HomeController(mockCalculator.Object);
+            var model = new CalculatorModel { X = x.ToString(), Y = y.ToString() };
+
+            mockCalculator.Setup(c => c.Subtract(x, y)).Returns(result);
+
+            // 2. Act
+
+            var viewResult = (homeController.Subtract(model) as ViewResult);
+
+            // 3. Assert
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsNotNull(viewResult.Model);
+
+            model = (viewResult.Model as CalculatorModel);
+
+            Assert.IsNotNull(model);
+            Assert.AreEqual(model.Result, result.ToString());
+        }
+
+        [Test]
+        public void Should_raise_vaildation_error_when_subtracting_where_X_is_not_a_number()
+        {
+            // 1. Arrange
+
+            double y = 3;
+
+            var mockCalculator = new Mock<ICalculator>();
+            var homeController = new HomeController(mockCalculator.Object);
+            var calculatorModel = new CalculatorModel { X = "foo", Y = y.ToString() };
+
+            // 2. Act
+
+            var viewResult = (homeController.Subtract(calculatorModel) as ViewResult);
+
+            // 3. Assert
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(viewResult.ViewData.ModelState.Count, 1);
+
+            var modelState = viewResult.ViewData.ModelState.Values.First();
+
+            Assert.AreEqual(modelState.Errors.Count, 1);
+            Assert.AreEqual(modelState.Errors.First().ErrorMessage, "X must be a number.");
+        }
+
+        [Test]
+        public void Should_raise_vaildation_error_when_subtracting_where_Y_is_not_a_number()
+        {
+            // 1. Arrange
+
+            double x = 3;
+
+            var mockCalculator = new Mock<ICalculator>();
+            var homeController = new HomeController(mockCalculator.Object);
+            var calculatorModel = new CalculatorModel { X = x.ToString(), Y = "bar" };
+
+            // 2. Act
+
+            var viewResult = (homeController.Subtract(calculatorModel) as ViewResult);
+
+            // 3. Assert
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(viewResult.ViewData.ModelState.Count, 1);
+
+            var modelState = viewResult.ViewData.ModelState.Values.First();
+
+            Assert.AreEqual(modelState.Errors.Count, 1);
+            Assert.AreEqual(modelState.Errors.First().ErrorMessage, "Y must be a number.");
+        }
+
+        [Test]
+        public void Should_multiply_two_numbers()
+        {
+            // 1. Arrange
+
+            double x = 3;
+            double y = 2;
+            var result = (x * y);
+            var mockCalculator = new Mock<ICalculator>();
+            var homeController = new HomeController(mockCalculator.Object);
+            var model = new CalculatorModel { X = x.ToString(), Y = y.ToString() };
+
+            mockCalculator.Setup(c => c.Multiply(x, y)).Returns(result);
+
+            // 2. Act
+
+            var viewResult = (homeController.Multiply(model) as ViewResult);
+
+            // 3. Assert
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsNotNull(viewResult.Model);
+
+            model = (viewResult.Model as CalculatorModel);
+
+            Assert.IsNotNull(model);
+            Assert.AreEqual(model.Result, result.ToString());
+        }
+
+        [Test]
+        public void Should_raise_vaildation_error_when_multiplying_where_X_is_not_a_number()
+        {
+            // 1. Arrange
+
+            double y = 3;
+
+            var mockCalculator = new Mock<ICalculator>();
+            var homeController = new HomeController(mockCalculator.Object);
+            var calculatorModel = new CalculatorModel { X = "foo", Y = y.ToString() };
+
+            // 2. Act
+
+            var viewResult = (homeController.Multiply(calculatorModel) as ViewResult);
+
+            // 3. Assert
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(viewResult.ViewData.ModelState.Count, 1);
+
+            var modelState = viewResult.ViewData.ModelState.Values.First();
+
+            Assert.AreEqual(modelState.Errors.Count, 1);
+            Assert.AreEqual(modelState.Errors.First().ErrorMessage, "X must be a number.");
+        }
+
+        [Test]
+        public void Should_raise_vaildation_error_when_multiplying_where_Y_is_not_a_number()
+        {
+            // 1. Arrange
+
+            double x = 3;
+
+            var mockCalculator = new Mock<ICalculator>();
+            var homeController = new HomeController(mockCalculator.Object);
+            var calculatorModel = new CalculatorModel { X = x.ToString(), Y = "bar" };
+
+            // 2. Act
+
+            var viewResult = (homeController.Multiply(calculatorModel) as ViewResult);
+
+            // 3. Assert
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(viewResult.ViewData.ModelState.Count, 1);
+
+            var modelState = viewResult.ViewData.ModelState.Values.First();
+
+            Assert.AreEqual(modelState.Errors.Count, 1);
+            Assert.AreEqual(modelState.Errors.First().ErrorMessage, "Y must be a number.");
+        }
+
+        [Test]
+        public void Should_divide_two_numbers()
+        {
+            // 1. Arrange
+
+            double x = 3;
+            double y = 2;
+            var result = (x / y);
+            var mockCalculator = new Mock<ICalculator>();
+            var homeController = new HomeController(mockCalculator.Object);
+            var model = new CalculatorModel { X = x.ToString(), Y = y.ToString() };
+
+            mockCalculator.Setup(c => c.Divide(x, y)).Returns(result);
+
+            // 2. Act
+
+            var viewResult = (homeController.Divide(model) as ViewResult);
+
+            // 3. Assert
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsNotNull(viewResult.Model);
+
+            model = (viewResult.Model as CalculatorModel);
+
+            Assert.IsNotNull(model);
+            Assert.AreEqual(model.Result, result.ToString());
+        }
+
+        [Test]
+        public void Should_raise_vaildation_error_when_dividing_where_X_is_not_a_number()
+        {
+            // 1. Arrange
+
+            double y = 3;
+
+            var mockCalculator = new Mock<ICalculator>();
+            var homeController = new HomeController(mockCalculator.Object);
+            var calculatorModel = new CalculatorModel { X = "foo", Y = y.ToString() };
+
+            // 2. Act
+
+            var viewResult = (homeController.Divide(calculatorModel) as ViewResult);
+
+            // 3. Assert
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(viewResult.ViewData.ModelState.Count, 1);
+
+            var modelState = viewResult.ViewData.ModelState.Values.First();
+
+            Assert.AreEqual(modelState.Errors.Count, 1);
+            Assert.AreEqual(modelState.Errors.First().ErrorMessage, "X must be a number.");
+        }
+
+        [Test]
+        public void Should_raise_vaildation_error_when_dividing_where_Y_is_not_a_number()
+        {
+            // 1. Arrange
+
+            double x = 3;
+
+            var mockCalculator = new Mock<ICalculator>();
+            var homeController = new HomeController(mockCalculator.Object);
+            var calculatorModel = new CalculatorModel { X = x.ToString(), Y = "bar" };
+
+            // 2. Act
+
+            var viewResult = (homeController.Divide(calculatorModel) as ViewResult);
+
+            // 3. Assert
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(viewResult.ViewData.ModelState.Count, 1);
+
+            var modelState = viewResult.ViewData.ModelState.Values.First();
+
+            Assert.AreEqual(modelState.Errors.Count, 1);
+            Assert.AreEqual(modelState.Errors.First().ErrorMessage, "Y must be a number.");
+        }
+    }
+}
